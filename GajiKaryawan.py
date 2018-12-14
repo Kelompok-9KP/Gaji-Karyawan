7284ddf8e76a8758df614cabb40e41a3e2fdf8a1
dwi-nugroho <nugrohoarjuna49@gmail.com>
2018-12-14 21:06:02 +0700

    second commit

--- GajiKaryawan.py
+++ GajiKaryawan.py
@@ -1,3 +1,14 @@
 print ("=========================================")
 print ("      Aplikasi Penggajian Karyawan")
 print ("=========================================")
+
+nama = str(input("masukan nama pegawai :"))
+golongan = str(input("golongan" :"))
+awal = int(input("tahun awal bekerja :"))
+akhir = int(input(habis kontrak :"))
+jam = int(input("jam per hari bekerja :")
+anak = int("jmlh anak :"))
+kerja = 7+jam
+tgb = (hari*jam)*(anak*25000)
+tgt = tgb*12
+tga = tgt*(akhir-awal)
