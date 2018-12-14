43ada06a613947ebfd57990dee76f35e2a128338
lintang3112 <lintangpermadi35@gmail.com>
2018-12-14 16:35:19 +0700

    first commit

--- GajiKaryawan.py
+++ GajiKaryawan.py
@@ -1,9 +1,3 @@
 print ("=========================================")
 print ("      Aplikasi Penggajian Karyawan")
 print ("=========================================")
-
-nama = str(input("Masukan Nama pegawai: "))
-Golongan = str(input("Golongan: "))
-awal = int(input("Tahun awal bekerja: "))
-akhir = int(input("Habis kontrak: "))
-jam = int(input("Jam per hari bekerja: "))
