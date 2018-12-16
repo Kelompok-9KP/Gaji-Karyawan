a7c2b12cf724438bf8989296c1624a86c90f67a8
lintang3112 <lintangpermadi35@gmail.com>
2018-12-16 14:16:52 +0700

    three commit

--- GajiKaryawan.py
+++ GajiKaryawan.py
@@ -12,3 +12,16 @@ kerja = 7+jam
 tgb = (hari*jam)*(anak*25000)
 tgt = tgb*12
 tga = tgt*(akhir-awal)
+
+if Golongan == "A":
+	print("jabatan = Direktur")
+	print("Gapok = 3000000")
+elif Golongan == "B":
+	print("Jabatan = Manager")
+	print("Gapok = 2500000")
+elif Golongan == "C":
+	print("Jabatan = Superviser")
+	print("Gapok = 2000000")
+elif Golongan == "D":
+	print("Jabatan = Operator")
+	print("Gapok = 1500000")
